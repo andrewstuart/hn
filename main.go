16ba07a73bf04ea42bbd5b91c383396887f53f88
Andrew Stuart <andrew.stuart2@gmail.com>
2014-05-10 19:56:26 -0700

    Finished scraper implementation.

--- main.go
+++ main.go
@@ -3,15 +3,41 @@ package main
 import (
   "log"
   "github.com/PuerkitoBio/goquery"
+  "fmt"
   "code.google.com/p/goncurses"
   "net/http"
   "crypto/tls"
+  "strings"
+  "strconv"
 )
+
+const YCRoot = "https://news.ycombinator.com"
+const rowsPerArticle = 3
+
 var scr *goncurses.Window
 var doc *goquery.Document
 var resp *http.Response
 var e error
 
+//Comments structure for HN articles
+type Comment struct {
+  Text string `json:"text"`
+  User string `json:"user"`
+  Id int `json:"id"`
+  Children []*Comment `json:"children"`
+}
+
+//Article structure
+type Article struct {
+  Title string `json:"title"xml:"`
+  Points int `json:"points"`
+  Id int `json:"id"`
+  Url string `json:"url"`
+  SiteLabel string `json:"siteLabel"`
+  Comments []*Comment `json:"comments"`
+  User string `json:"user"`
+  Created string `json:"created"`
+}
 
 func main() {
   if scr, e = goncurses.Init(); e != nil {
@@ -25,19 +51,97 @@ func main() {
 
   client := &http.Client{Transport: trans}
 
-  if resp, e = client.Get("https://news.ycombinator.com/news"); e != nil {
-    log.Print(e)
-  }
+  next := YCRoot + "/news"
+  exit := false
 
-  if doc, e = goquery.NewDocumentFromResponse(resp); e != nil {
-    log.Fatal(e)
-  }
+  ars := make([]*Article, 0)
+  page := 0
 
-  doc.Find("tbody td.title a:first-child").Each(func(i int, s *goquery.Selection) {
-    if t := s.Text(); t != "More" {
-      scr.Printf("%d. %s\n", i + 1, t)
+
+  for !exit {
+
+    if resp, e = client.Get(next); e != nil {
+      log.Print(e)
     }
-  })
-  scr.Refresh()
-  scr.GetChar()
+
+    if doc, e = goquery.NewDocumentFromResponse(resp); e != nil {
+      log.Fatal(e)
+    }
+
+
+    rows := doc.Find("tr").Eq(3).Find("tr")
+
+    nextHref, _ := rows.Last().Find("a").Attr("href")
+
+    if nextHref[0] == '/' {
+      next = YCRoot + nextHref
+    } else {
+      next = YCRoot + "/" + nextHref
+    }
+
+    rows.Each(func(i int, row *goquery.Selection) {
+      if i % rowsPerArticle > 0 || (len(ars) % 30 == 0 && i != 0){ return }
+
+      ar := Article{}
+      up := row.Find("td.title").First().Next()
+
+      if idSt, exists := up.Find("a").First().Attr("id"); exists {
+        if id, err := strconv.Atoi(strings.Split(idSt, "_")[1]); err == nil {
+          ar.Id = id
+        } else {
+          fmt.Println(idSt)
+          log.Fatal(err)
+        }
+      }
+
+      title := up.Next()
+      link := title.Find("a").First()
+
+      ar.Title = link.Text()
+      if url, exists := link.Attr("href"); exists {
+        ar.Url = url
+      }
+
+      ar.SiteLabel = title.Find("span.comhead").Text()
+
+      row = row.Next()
+
+      row.Find("span").Each(func (i int, s *goquery.Selection) {
+        if pts, err := strconv.Atoi(strings.Split(s.Text(), " ")[0]); err == nil {
+          ar.Points = pts
+        } else {
+          fmt.Println(ar)
+          fmt.Println(row.Html())
+          log.Fatal(err)
+        }
+      })
+
+      ar.User = row.Find("td.subtext a:first-child").Text()
+
+      ars = append(ars, &ar)
+
+      //scr.Print(ar)
+      //scr.Refresh()
+      //scr.GetChar()
+    })
+
+    scr.Clear()
+
+    start := 30 * page
+    end := start + 30
+
+    for i, ar := range ars[start:end] {
+      scr.Printf("%d. (%d): %s\n", start + i + 1, ar.Points, ar.Title)
+    }
+
+    scr.Print("\n\nPress n to continue or q to quit\n\n")
+    scr.Refresh()
+
+    switch goncurses.KeyString(scr.GetChar()) {
+    case "q":
+      exit = true
+    default:
+      page += 1
+    }
+  }
 }
