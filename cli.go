450148ce687a3078347812f7b941333ee5a51ea4
Andrew Stuart <andrew.stuart2@gmail.com>
2014-07-31 21:09:05 -0700

    Almost done

--- cli.go
+++ cli.go
@@ -6,13 +6,13 @@ import (
 	"strconv"
 )
 
+var input string = ""
+
 func storyHandler(ch string) {
-	input := ""
 	switch ch {
 	case "c":
 		if num, err := strconv.Atoi(input); err == nil {
 			if num < 1 {
-				doneWithInput = true
 				break
 			}
 
@@ -21,26 +21,13 @@ func storyHandler(ch string) {
 			}
 
 			text := p.Articles[num-1].PrintComments()
-			line := 0
 
-			cont := true
-			for cont {
-				a := scr.GetChar()
-
-				// Verify lines are not negative. Bad mojo
-				if line < 0 {
-					line = 0
-				}
-			}
-
-			doneWithInput = true
+			cli.SetContent(text)
+			input = ""
 		} else {
-			scr.Clear()
-			scr.Print("\n\nPlease enter a number to select a comment\n\n")
-			scr.Refresh()
-			scr.GetChar()
-			doneWithInput = true
+			cli.Alert("Please enter a number to select a comment")
 		}
+		break
 	case "o":
 		if num, err := strconv.Atoi(input); err == nil {
 			for num-1 > len(p.Articles) {
@@ -49,39 +36,36 @@ func storyHandler(ch string) {
 
 			viewInBrowser := exec.Command("xdg-open", p.Articles[num-1].Url)
 			viewInBrowser.Start()
-			doneWithInput = true
 		} else {
-			scr.Clear()
-			scr.Print("\n\nPlease enter a number to view an article\n\n")
-			scr.Refresh()
-			doneWithInput = true
+			cli.Alert("Please enter a number to view an article")
 		}
+		input = ""
+		break
 	case "q":
-		doneWithInput = true
-		exit = true
+		cli.Quit()
+		break
 	case "n":
+		//Go forward 1 page
 		pageNum += 1
-		doneWithInput = true
+		cli.SetContent(getStories(pageNum))
+		break
 	case "p":
+		//Go back 1 page, unless page < 0
 		if pageNum > 0 {
 			pageNum -= 1
 		}
-		doneWithInput = true
+		cli.SetContent(getStories(pageNum))
+		break
 	case "enter":
-		continue
+		break
 	case "backspace":
-		//Not the prettiest but whatever
-		cy, cx := scr.CursorYX()
 		if len(input) > 0 {
 			input = input[:len(input)-1]
-			scr.MoveDelChar(cy, cx-3)
-			scr.DelChar()
-			scr.DelChar()
+			cli.DelChar()
 		} else {
-			scr.MoveDelChar(cy, cx-2)
-			scr.DelChar()
-			scr.DelChar()
+			cli.DelChar()
 		}
+		break
 	default:
 		input += ch
 		break
@@ -101,49 +85,59 @@ func commentHandler(input string) {
 		cli.Scroll(1)
 		break
 	case "k":
-		cli.Scroll(1)
+		cli.Scroll(-1)
 		break
 	case "n":
 		cli.Scroll(cli.Height)
 		break
 	case "p":
-		cli.Scroll(cli.Height)
+		cli.Scroll(-cli.Height)
 		break
 	case "q":
-		cli.SetKeyHandler(storyHandler)
+		cli.ResetScroll()
 		cli.SetContent(stories)
-		break
+		cli.SetKeyHandler(storyHandler)
+		return
 	}
 }
 
 var cli hncli
+var p *PageCache
+
+var stories string
+
+func getStories(pageNum int) string {
+	h := cli.Height
+
+	start = h * pageNum
+	end = start + h
+
+	for end > len(p.Articles) {
+		p.GetNext()
+	}
+
+	str := ""
+	for i, ar := range p.Articles[start:end] {
+		str += fmt.Sprintf("%d. (%d): %s\n", start+i+1, ar.Karma, ar.Title)
+	}
+
+	return str
+}
+
+var pageNum, start, end int
 
 func runCli() {
+	pageNum = 0
 	cli = GetCli()
 
-	exit := false
-	pageNum := 0
-	p := NewPageCache()
+	p = NewPageCache()
 
-	for !exit {
-		h := cli.Height
+	stories = getStories(pageNum)
 
-		start := h * pageNum
-		end := start + h
+	cli.SetContent(stories)
+	cli.SetKeyHandler(storyHandler)
+	cli.SetHelp("\n(n: next, p: previous, <num>c: view comments, <num>o: open in browser, q: quit)  ")
 
-		for end > len(p.Articles) {
-			p.GetNext()
-		}
-
-		content := ""
-
-		for i, ar := range p.Articles[start:end] {
-			content += fmt.Sprintf("%d. (%d): %s\n", start+i+1, ar.Karma, ar.Title)
-		}
-
-		cli.PrintHelp("\n(n: next, p: previous, <num>c: view comments, <num>o: open in browser, q: quit)  ")
-
-		cli.Refresh()
-
-	}
+	cli.Refresh()
+	cli.Run()
 }
--- hncli.go
+++ hncli.go
@@ -8,24 +8,28 @@ import (
 )
 
 type hncli struct {
-	root, main, help *goncurses.Window
-	Height           int
-	handler          CharHandler
-	finished         bool
+	root, main, help  *goncurses.Window
+	Height, pos       int
+	handler           CharHandler
+	finished          bool
+	helpText, content string
 }
 
-func (h hncli) Refresh() {
+func (h *hncli) Refresh() {
 	h.root.Refresh()
+	h.root.Move(h.Height-1, 0)
 }
 
 const MENU_HEIGHT int = 3
 
 var hc hncli
-var instantiated = false
+
+//Ha.
+var singleDone = false
 
 //Returns an instance of the CLI. Call once
 func GetCli() hncli {
-	if !instantiated {
+	if !singleDone {
 		hc = hncli{}
 
 		root, e := goncurses.Init()
@@ -43,39 +47,58 @@ func GetCli() hncli {
 
 		hc.Height = h - MENU_HEIGHT
 
-		instantiated = true
+		singleDone = true
 	}
 
 	return hc
 }
 
-func (h hncli) SetContent(format string, args ...interface{}) {
-	h.main.Printf(format, args...)
+func (h *hncli) SetContent(text string) {
+	h.content = text
+	h.main.Clear()
+
+	h.main.Printf(text)
+	h.main.Refresh()
 }
 
-func (h hncli) SetHelp(text string) {
+func (h *hncli) SetHelp(text string) {
+	h.helpText = text
+	h.help.Clear()
 	h.help.Print(text)
+	h.help.Refresh()
 }
 
 //Scroll the content that was added with SetContent
-func (h hncli) Scroll(amount int) {
+func (h *hncli) Scroll(amount int) {
+	newPos := h.pos + amount
 
+	if newPos < 0 {
+		h.pos = 0
+	} else {
+		h.pos = newPos
+	}
+
+	h.SetContent(h.paginate())
+}
+
+func (h *hncli) ResetScroll() {
+	h.pos = 0
 }
 
 type CharHandler func(string)
 
-func (h hncli) SetKeyHandler(CharHandler) {
-	h.handler = CharHandler
+func (h *hncli) SetKeyHandler(hand CharHandler) {
+	h.handler = hand
 }
 
-func (h hncli) Quit() {
+func (h *hncli) Quit() {
 	goncurses.End()
 	h.finished = true
 }
 
-func (h hncli) Run() {
-	for !finished {
-		c := scr.GetChar()
+func (h *hncli) Run() {
+	for !h.finished {
+		c := h.root.GetChar()
 
 		if c == 127 {
 			c = goncurses.Key(goncurses.KEY_BACKSPACE)
@@ -87,10 +110,25 @@ func (h hncli) Run() {
 	}
 }
 
-func (h hncli) getFitLines(s string) []string {
+//Show an alert, wait for a character, then reset
+func (h *hncli) Alert(text string) {
+	hText := h.helpText
+	h.SetHelp(text)
+	h.root.GetChar()
+	h.SetHelp(hText)
+}
+
+func (h *hncli) DelChar() {
+	cy, cx := h.root.CursorYX()
+	h.root.MoveDelChar(cy, cx-3)
+	h.root.DelChar()
+	h.root.DelChar()
+}
+
+func (h *hncli) getFitLines() []string {
 	_, w := h.main.MaxYX()
 
-	a := strings.Split(s, "\n")
+	a := strings.Split(h.content, "\n")
 
 	p := make([]string, 0, len(a))
 
@@ -120,19 +158,19 @@ func (h hncli) getFitLines(s string) []string {
 	return p
 }
 
-func (hc hncli) paginate(t string, n int) string {
+func (hc *hncli) paginate() string {
 	h, _ := hc.main.MaxYX()
 
-	lines := hc.getFitLines(t)
+	lines := hc.getFitLines()
 
 	nLines := len(lines)
 
 	//If text won't fit
 	if nLines > h {
 		//Determine start point
-		if n < nLines-h {
+		if hc.pos < nLines-h {
 			//If n is not in last h elements, reslice starting at n
-			lines = lines[n : n+h]
+			lines = lines[hc.pos : hc.pos+h]
 		} else {
 			//Else, Get last h (at most) elements
 			lines = lines[nLines-h:]
